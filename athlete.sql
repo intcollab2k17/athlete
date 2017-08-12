7f8b485eef3414d5b244d43010934f07feddf74c
Emoblazz <emoblazz@gmail.com>
2017-08-12 09:04:17 +0800

    database

---
+++ athlete.sql
@@ -0,0 +1,663 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 12, 2017 at 03:03 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `athlete`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE `album` (
+  `album_id` int(11) NOT NULL,
+  `album_name` varchar(40) NOT NULL,
+  `album_description` varchar(150) NOT NULL,
+  `date_posted` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album_content`
+--
+
+CREATE TABLE `album_content` (
+  `album_content_id` int(11) NOT NULL,
+  `album_id` varchar(11) NOT NULL,
+  `file` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `athlete`
+--
+
+CREATE TABLE `athlete` (
+  `athlete_id` int(11) NOT NULL,
+  `sports_id` varchar(100) NOT NULL,
+  `settings_id` int(10) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `coach_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `uniform` int(11) NOT NULL,
+  `remarks` varchar(500) NOT NULL,
+  `scholar_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `athlete`
+--
+
+INSERT INTO `athlete` (`athlete_id`, `sports_id`, `settings_id`, `member_id`, `coach_id`, `event_id`, `uniform`, `remarks`, `scholar_status`) VALUES
+(6, '3', 1, 7, 3, 0, 0, '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `award`
+--
+
+CREATE TABLE `award` (
+  `award_id` int(11) NOT NULL,
+  `athlete_id` int(11) NOT NULL,
+  `award` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `award`
+--
+
+INSERT INTO `award` (`award_id`, `athlete_id`, `award`) VALUES
+(1, 6, 'MVP');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow`
+--
+
+CREATE TABLE `borrow` (
+  `borrow_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `equipment_id` int(11) NOT NULL,
+  `borrow_qty` int(11) NOT NULL,
+  `date_borrowed` datetime NOT NULL,
+  `date_returned` datetime NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campus`
+--
+
+CREATE TABLE `campus` (
+  `campus_id` int(11) NOT NULL,
+  `campus` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `campus`
+--
+
+INSERT INTO `campus` (`campus_id`, `campus`) VALUES
+(1, 'Talisay'),
+(2, 'Fortune Towne');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`) VALUES
+(1, 'Memorandum'),
+(2, 'Resolution'),
+(3, 'Communication');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coach`
+--
+
+CREATE TABLE `coach` (
+  `coach_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `sports_id` int(11) NOT NULL,
+  `settings_id` int(10) NOT NULL,
+  `event_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `coach`
+--
+
+INSERT INTO `coach` (`coach_id`, `member_id`, `sports_id`, `settings_id`, `event_id`) VALUES
+(3, 8, 3, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` int(11) NOT NULL,
+  `course` varchar(10) NOT NULL,
+  `course_title` varchar(100) NOT NULL,
+  `course_status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_id`, `course`, `course_title`, `course_status`) VALUES
+(2, 'BSIS', 'Bachelor of Science in Information Systems', ''),
+(3, 'BSIT', 'Bachelor of Science in Industrial Technology', ''),
+(4, 'BEED', 'Bachelor of Science in Elementary Education', ''),
+(5, 'BSED', 'Bachelor of Science in Secondary Education', ''),
+(6, 'BSHRM', 'Bachelor of Science in Hotel and Restaurant Management', ''),
+(7, 'BSCE', 'Bachelor of Science in Civil Engineering', ''),
+(8, 'NA', 'Not Applicable\r\n', ''),
+(9, 'BSCS', 'Comsci', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `document`
+--
+
+CREATE TABLE `document` (
+  `document_id` int(11) NOT NULL,
+  `sender` varchar(30) NOT NULL,
+  `receiver` varchar(30) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `file` varchar(100) NOT NULL,
+  `date_uploaded` datetime NOT NULL,
+  `type` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment`
+--
+
+CREATE TABLE `equipment` (
+  `equipment_id` int(11) NOT NULL,
+  `equipment_name` varchar(100) NOT NULL,
+  `equipment_desc` varchar(1000) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `equipment`
+--
+
+INSERT INTO `equipment` (`equipment_id`, `equipment_name`, `equipment_desc`, `qty`) VALUES
+(1, 'Volleyball', 'Volleyball ', 6),
+(2, 'Volleyball Net', 'Volleyball Net Black', 3),
+(3, 'Rocket', 'Badminton Rocket', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `event_id` int(11) NOT NULL,
+  `event_name` varchar(100) NOT NULL,
+  `event_status` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `event`
+--
+
+INSERT INTO `event` (`event_id`, `event_name`, `event_status`) VALUES
+(1, 'Intramurals 2017', 'active'),
+(2, 'Quadra 2017', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `member_id` int(11) NOT NULL,
+  `member_last` varchar(30) NOT NULL,
+  `member_first` varchar(30) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `ys` varchar(10) NOT NULL,
+  `member_type` varchar(10) NOT NULL,
+  `course` varchar(15) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `bday` date NOT NULL,
+  `campus_id` int(11) NOT NULL,
+  `member_status` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`member_id`, `member_last`, `member_first`, `gender`, `ys`, `member_type`, `course`, `address`, `bday`, `campus_id`, `member_status`) VALUES
+(7, 'Aboy', 'Kenneth', 'Male', '4B', 'Student', 'BSIS', 'Silay City', '0000-00-00', 1, 1),
+(8, 'Cango', 'Rogelio', 'Male', 'NA', 'Faculty', 'NA', 'Talisay City', '0000-00-00', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sched`
+--
+
+CREATE TABLE `sched` (
+  `sched_id` int(11) NOT NULL,
+  `program` varchar(100) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scholars`
+--
+
+CREATE TABLE `scholars` (
+  `scholar_id` int(11) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `middlename` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `birthday` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `course` varchar(10) NOT NULL,
+  `year_sec` varchar(5) NOT NULL,
+  `campus_id` int(20) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `contact_number` varchar(15) NOT NULL,
+  `contact_person` varchar(30) NOT NULL,
+  `contact_person_number` varchar(20) NOT NULL,
+  `scholarship_id` int(12) NOT NULL,
+  `file_profile` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sem`
+--
+
+CREATE TABLE `sem` (
+  `sem_id` int(11) NOT NULL,
+  `semester` varchar(20) NOT NULL,
+  `current_sem` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sem`
+--
+
+INSERT INTO `sem` (`sem_id`, `semester`, `current_sem`) VALUES
+(1, '1st', 'Yes'),
+(2, '2nd', 'No'),
+(3, 'summer', 'No'),
+(4, 'sasa', 'No');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `settings_id` int(11) NOT NULL,
+  `sem` varchar(15) NOT NULL,
+  `sy` varchar(30) NOT NULL,
+  `status` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`settings_id`, `sem`, `sy`, `status`) VALUES
+(4, '1st', '2017-2018', ''),
+(5, '2nd', '2017-2018', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sports`
+--
+
+CREATE TABLE `sports` (
+  `sports_id` int(11) NOT NULL,
+  `sports_name` varchar(100) NOT NULL,
+  `sports_status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sports`
+--
+
+INSERT INTO `sports` (`sports_id`, `sports_name`, `sports_status`) VALUES
+(3, 'Swimming', 'active'),
+(4, 'Volleyball', 'active'),
+(5, 'saa', 'active'),
+(6, 'sss', 'inactive'),
+(7, 'dd', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sy`
+--
+
+CREATE TABLE `sy` (
+  `sy_id` int(11) NOT NULL,
+  `sy` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sy`
+--
+
+INSERT INTO `sy` (`sy_id`, `sy`) VALUES
+(2, '2006-2007'),
+(1, '2017-2018');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `designation` varchar(30) NOT NULL,
+  `campus_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password`, `name`, `status`, `designation`, `campus_id`) VALUES
+(2, 'admin', 'password', 'Kaye Merida', '', 'director', 0),
+(3, 'admin', 'admin', 'jez pez', '', 'Sports Director', 0),
+(4, 'selena', 'pass', 'selena gomez', '', '', 0),
+(5, 'ken', '123', 'Kenneth Aboy', '', 'Sports Coordinator', 0),
+(6, 'a', 'a', 'dsds', '', 'Sports Director', 0),
+(7, 'a', 'aa', 'qq', '', 'Sports Director', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`album_id`);
+
+--
+-- Indexes for table `album_content`
+--
+ALTER TABLE `album_content`
+  ADD PRIMARY KEY (`album_content_id`);
+
+--
+-- Indexes for table `athlete`
+--
+ALTER TABLE `athlete`
+  ADD PRIMARY KEY (`athlete_id`);
+
+--
+-- Indexes for table `award`
+--
+ALTER TABLE `award`
+  ADD PRIMARY KEY (`award_id`);
+
+--
+-- Indexes for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD PRIMARY KEY (`borrow_id`);
+
+--
+-- Indexes for table `campus`
+--
+ALTER TABLE `campus`
+  ADD PRIMARY KEY (`campus_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `coach`
+--
+ALTER TABLE `coach`
+  ADD PRIMARY KEY (`coach_id`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `document`
+--
+ALTER TABLE `document`
+  ADD PRIMARY KEY (`document_id`);
+
+--
+-- Indexes for table `equipment`
+--
+ALTER TABLE `equipment`
+  ADD PRIMARY KEY (`equipment_id`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`member_id`);
+
+--
+-- Indexes for table `sched`
+--
+ALTER TABLE `sched`
+  ADD PRIMARY KEY (`sched_id`);
+
+--
+-- Indexes for table `scholars`
+--
+ALTER TABLE `scholars`
+  ADD PRIMARY KEY (`scholar_id`);
+
+--
+-- Indexes for table `sem`
+--
+ALTER TABLE `sem`
+  ADD PRIMARY KEY (`sem_id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`settings_id`);
+
+--
+-- Indexes for table `sports`
+--
+ALTER TABLE `sports`
+  ADD PRIMARY KEY (`sports_id`);
+
+--
+-- Indexes for table `sy`
+--
+ALTER TABLE `sy`
+  ADD PRIMARY KEY (`sy_id`),
+  ADD UNIQUE KEY `sy` (`sy`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `album`
+--
+ALTER TABLE `album`
+  MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `album_content`
+--
+ALTER TABLE `album_content`
+  MODIFY `album_content_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `athlete`
+--
+ALTER TABLE `athlete`
+  MODIFY `athlete_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `award`
+--
+ALTER TABLE `award`
+  MODIFY `award_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `borrow`
+--
+ALTER TABLE `borrow`
+  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `campus`
+--
+ALTER TABLE `campus`
+  MODIFY `campus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `coach`
+--
+ALTER TABLE `coach`
+  MODIFY `coach_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `document`
+--
+ALTER TABLE `document`
+  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `equipment`
+--
+ALTER TABLE `equipment`
+  MODIFY `equipment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `sched`
+--
+ALTER TABLE `sched`
+  MODIFY `sched_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `scholars`
+--
+ALTER TABLE `scholars`
+  MODIFY `scholar_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sem`
+--
+ALTER TABLE `sem`
+  MODIFY `sem_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `settings_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `sports`
+--
+ALTER TABLE `sports`
+  MODIFY `sports_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `sy`
+--
+ALTER TABLE `sy`
+  MODIFY `sy_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
